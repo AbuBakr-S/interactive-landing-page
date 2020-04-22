4bb1af781952f0659411aaa60e385a8513ccd3d0
Abubakar Siddique <abz.siddique@gmail.com>
2020-04-22 18:40:12 +0100

    Revert "feat: Use a single click event listener"
    This reverts commit 95e02f5776548d1acd565ceff497a3cbf2df6673.

--- js/app.js
+++ js/app.js
@@ -34,9 +34,11 @@ window.addEventListener('DOMContentLoaded', () => {
         console.log(anchor);
         document.getElementById(anchor).scrollIntoView({ behavior: 'smooth'});
     }
-
-    // Add a single event listener to the <ul>
-    navbarList.addEventListener('click', scrollToLinkSection);
+      
+    // Loop through each anchor and add an event listener
+    document.querySelectorAll('a').forEach((elem) => {
+        elem.addEventListener('click', scrollToLinkSection);
+    });
 
 
     // Hide navigation bar when scrolling
